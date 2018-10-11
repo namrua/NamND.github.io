e8a085e650f7c1ad5b20311bbbecf27dbc838486
namrua <moonloren9x@gmail.com>
2018-10-12 01:54:48 +0700

    Done
    Done

---
+++ video.js
@@ -0,0 +1,9 @@
+$("body").vide("video1.mp4",{
+    volume: 1,
+    playbackRate: 1,
+    muted: true,
+    loop: true,
+    autoplay: true,
+    posterType: 'detect', // Poster image type. "detect" — auto-detection; "none" — no poster; "jpg", "png", "gif",... - extensions.
+    resizing: true
+});
